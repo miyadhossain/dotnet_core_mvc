96cba9d4abcf76e9ea1f643bc904a14d1f33621c
miyadhossain <miyad.mh@gmail.com>
2022-12-15 16:36:54 +0600

    mystore added

---
+++ Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            clientInfo.name = Request.Form["name"];
+            clientInfo.name = Request.Form["email"];
+            clientInfo.name = Request.Form["phone"];
+            clientInfo.name = Request.Form["address"];
+
+            //if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 || clientInfo.address.Length == 0) 
+            
+            //{
+            //    errorMessage = "All the field are required";
+            //    return;
+            //}
+
+            // Save the new clinet into the database
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO clients " +
+                        "(name, email, phone, address) VALUES " +
+                        "(@name, @email, @phone, @address);";
+
+                    using(SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+            clientInfo.name = ""; clientInfo.email = ""; clientInfo.phone = ""; clientInfo.address = "";
+            successMessage = "New Client Added Successfull!";
+
+            Response.Redirect("/Clients/Index");
+        }
+    }
+}
